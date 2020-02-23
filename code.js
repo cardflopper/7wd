a23e9a803a058626f52d8460f6b2fbff3ed46d2c
cardflopper <smchacko@gmail.com>
2020-02-23 12:11:10 -0500

    Update code.js

--- code.js
+++ code.js
@@ -3,42 +3,6 @@ let tokens = ['agr','arc','eco','law','mas','mat','phi','str','the','urb'];
 randomize();
 
 function randomize(){
-
-    let output = document.getElementById("output");
-    output.hidden=true;
-    while (output.firstChild) {
-        output.removeChild(output.lastChild);
-    }    
-    shuffleArray(tokens)
-
-    let chosen = tokens.slice(0,5);
-   
-    
-    for(let i=0; i<chosen.length;i++){
-        let pic = document.createElement("img");
-        pic.setAttribute("src","img/progress_token_"+chosen[i]+"_cut.png");
-        pic.classList.add("token");
-        output.append(pic);
-    }
-    output.hidden=false;
-}
-
-/**
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- */
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }let tokens = ['agr','arc','eco','law','mas','mat','phi','str','the','urb'];
-
-
-randomize();
-
-function randomize(){
     let output = document.getElementById("output");
     resetAnimation(output);
     output.hidden = true;
